7b2deb28710c12559e0864a6745d733f3ee894e2
aayam07 <thisisleo7@gmail.com>
2025-05-22 07:13:14 +0000

    Add multipurpose hall images

--- frontend/src/httpCommon.js
+++ frontend/src/httpCommon.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "/api",
   // baseURL: "https://event-hall.onrender.com/",
   headers: {
     "Content-type": "application/json",
